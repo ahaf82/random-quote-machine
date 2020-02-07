107f2fbf75d9a0e05bb81971ef89ed3eabae0711
Armin Hafner <a_h82@gmx.net>
2020-02-07 22:58:25 +0100

    Updates

---
+++ precache-manifest.57dffa5cfa841419435bb7222f4b552a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16756977a669636e4935e5698d8d8e20",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "1045f5f4a52b3d1c67c9",
+    "url": "/random-quote-machine/static/css/main.23ca4894.chunk.css"
+  },
+  {
+    "revision": "885977cf89fd319f29b3",
+    "url": "/random-quote-machine/static/js/2.cbb021b7.chunk.js"
+  },
+  {
+    "revision": "6e498969aafde287c0e2e1a721426fda",
+    "url": "/random-quote-machine/static/js/2.cbb021b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1045f5f4a52b3d1c67c9",
+    "url": "/random-quote-machine/static/js/main.539f8fda.chunk.js"
+  },
+  {
+    "revision": "afc9b93c50eb3e83906a",
+    "url": "/random-quote-machine/static/js/runtime-main.daced29d.js"
+  }
+]);
